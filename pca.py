a2417eb45f44256516ab48d076ecc1c84aa8f027
ijiho <jlee120607@gmail.com>
2026-06-26 18:29:28 +0900

    - Added helper functions dot, norm, normalize, projection - Implemented Classical Gram-Schmidt & Verified it

--- pca.py
+++ pca.py
@@ -24,8 +24,54 @@ def covariance(X, Y):
     cov = np.sum(X_centered * Y_centered)/(X.shape[0]-1)
     return cov
 
+def dot(u, v):
+    assert u.shape == v.shape
+    result = 0
+    for i in range(u.shape[0]):
+        result += u[i] * v[i]
+    return result
+
+def norm(v):
+    squared_sum = 0
+    for i in range(v.shape[0]):
+        squared_sum += v[i] ** 2
+    return squared_sum ** 0.5
+
+def normalize(v):
+    norm_v = norm(v)
+    if norm_v < 1e-12:
+        raise ValueError("Zero Vector Detected")
+    return v / norm_v
+
+def projection(vector, onto):
+    return dot(vector, onto) / dot(onto, onto) * onto
+
+def gram_schmidt(A):
+    Q = [normalize(A[:, 0])]
+    for i in range(1, A.shape[1]):
+        a = A[:, i]
+        u = a.astype(float).copy()
+        for q in Q:
+            u -= projection(u, q)
+        q = normalize(u)
+        Q.append(q)
+    Q = np.column_stack(Q)
+    R = Q.T @ A
+    return Q, R
+
+# Verifying for Gram-Schmidt
+A = np.array([[1, 2], [3, 4]])
+Q, R = gram_schmidt(A)
+print(np.allclose(Q @ R, A))
+
+# Verifying Q orthogonality
+print(np.allclose(Q.T @ Q, np.eye(Q.shape[1])))
+
+# Verifying that R is triangular
+print(R)
+
 # PCA class
-class pca:
+class PCA:
     def __init__(self, n_components):
         self.n_components = n_components
     def standardize(self, X):
@@ -41,7 +87,6 @@ class pca:
                 cov = covariance(x, y)
                 cov_matrix[i][j] = cov
         return cov_matrix
-
 
 
 
